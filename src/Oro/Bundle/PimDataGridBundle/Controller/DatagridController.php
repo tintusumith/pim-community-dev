fc14db02893b69485f71c1c91e7c96a2871ed35f
Laurent Petard <laurent.petard@akeneo.com>
2018-09-14 17:14:23 +0200

    Simplify datagrid loading endpoint

--- src/Oro/Bundle/PimDataGridBundle/Controller/DatagridController.php
+++ src/Oro/Bundle/PimDataGridBundle/Controller/DatagridController.php
@@ -2,10 +2,11 @@
 
 namespace Oro\Bundle\PimDataGridBundle\Controller;
 
-use Oro\Bundle\DataGridBundle\Datagrid\MetadataParser;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Oro\Bundle\DataGridBundle\Datagrid\Manager;
+use Oro\Bundle\DataGridBundle\Datagrid\RequestParameters;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Datagrid controller
@@ -16,19 +17,30 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class DatagridController
 {
-    /**
-     * @var EngineInterface
-     */
-    protected $templating;
+    const DATAGRID_ROUTE = 'oro_datagrid_index';
+
+    /** @var Manager */
+    private $manager;
+
+    /** @var RequestParameters */
+    private $requestParams;
+
+    /** @var RouterInterface */
+    private $router;
 
     /**
-     * @param EngineInterface $templating
-     * @param MetadataParser  $metadata
+     * @param Manager           $manager
+     * @param RequestParameters $requestParams
+     * @param RouterInterface   $router
      */
-    public function __construct(EngineInterface $templating, MetadataParser $metadata)
-    {
-        $this->templating = $templating;
-        $this->metadata   = $metadata;
+    public function __construct(
+        Manager $manager,
+        RequestParameters $requestParams,
+        RouterInterface $router
+    ) {
+        $this->manager = $manager;
+        $this->requestParams = $requestParams;
+        $this->router = $router;
     }
 
     /**
@@ -42,10 +54,32 @@ class DatagridController
     public function loadAction(Request $request, $alias)
     {
         $params = $request->get('params', []);
+        $datagrid = $this->manager->getDatagrid($alias);
+        $metaData = $datagrid->getMetadata();
+
+        $metaData->offsetAddToArray('options', ['url' => $this->generateDatagridUrl($alias, $params)]);
 
         return new JsonResponse([
-            'metadata' => $this->metadata->getGridMetadata($alias, $params),
-            'data' => $this->metadata->getGridData($alias, $params)
+            'metadata' => $metaData->toArray(),
+            'data' => json_encode($datagrid->getData()->toArray()), // @todo: fix front to not json encode
         ]);
+    }
+
+    /**
+     * @param string $name
+     * @param array  $params
+     *
+     * @return string
+     */
+    protected function generateDatagridUrl(string $name, array $params): string
+    {
+        $additional = $this->requestParams->getRootParameterValue();
+
+        $params = [
+            $name      => array_merge($params, $additional),
+            'gridName' => $name
+        ];
+
+        return $this->router->generate(self::DATAGRID_ROUTE, $params);
     }
 }
